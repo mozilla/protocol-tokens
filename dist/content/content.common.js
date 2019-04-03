8e97fd2f7602f10265f1a2fcb348e6d169b174a3
Michael McCombie <michael@michaelm.cc>
2019-04-03 15:59:50 -0700

    Remove /dist from gitignoe

---
+++ dist/content/content.common.js
@@ -0,0 +1,13 @@
+module.exports = {
+  contentXs: "304px",
+  contentSm: "464px",
+  contentMd: "736px",
+  contentLg: "992px",
+  contentXl: "1248px",
+  contentMax: "1420px",
+  screenXs: "320px",
+  screenSm: "480px",
+  screenMd: "768px",
+  screenLg: "1056px",
+  screenXl: "1312px",
+};
---
+++ dist/font-stack/font-stack.common.js
@@ -0,0 +1,5 @@
+module.exports = {
+  fontStackSans: "'Open Sans', X-LocaleSpecific, sans-serif",
+  fontStackSerif: "'Zilla Slab', 'Open Sans', X-LocaleSpecific, serif",
+  fontStackMono: "Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+};
