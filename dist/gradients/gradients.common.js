718500c41b5775a74aabfe812aa906b32e092216
Michael McCombie <mvmccombie@gmail.com>
2019-04-03 16:52:02 -0700

    Remove /dist from gitignore (#42)

---
+++ dist/gradients/gradients.common.js
@@ -0,0 +1,5 @@
+module.exports = {
+  gradientGreyscale: "linear-gradient(90deg, #000000, #ffffff)",
+  gradientFocus: "linear-gradient(45deg, #4A1475, #671878, #C42482, #FF271D)",
+  gradientHelloworld: "radial-gradient(33deg, #4A1475, #671878, #C42482, #FF271D)",
+};
