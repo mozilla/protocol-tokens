a0400d31cbacb9436618f84e886f51dfd9d68934
Michael McCombie <michael@michaelm.cc>
2019-04-03 19:13:35 -0700

    Updated the dist folder
    Updates the dist folder to match the current token values.

--- dist/gradients/gradients.common.js
+++ dist/gradients/gradients.common.js
@@ -1,5 +1,5 @@
 module.exports = {
-  gradientGreyscale: "linear-gradient(90deg, #000000, #ffffff)",
-  gradientFocus: "linear-gradient(45deg, #4A1475, #671878, #C42482, #FF271D)",
-  gradientHelloworld: "radial-gradient(33deg, #4A1475, #671878, #C42482, #FF271D)",
+  gradientWarm: "linear-gradient(45deg, #F5156C, #FF3750, #FF4F5E, #FF7139, #FF980E)",
+  gradientCool: "linear-gradient(90deg, #3A8EE6, #9059FF, #C139E6)",
+  gradientHighlight: "linear-gradient(90deg, #FFFFFF, #FFF44F)",
 };
